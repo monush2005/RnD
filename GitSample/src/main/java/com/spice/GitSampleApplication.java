b37323a7ac98c1b3ee3d573b45bba3ea1d86ec11
monush2005 <monush2005@gmail.com>
2017-07-03 17:29:33 +0530

    3jul_6PM

---
+++ GitSample/src/main/java/com/spice/GitSampleApplication.java
@@ -0,0 +1,12 @@
+package com.spice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitSampleApplication.class, args);
+	}
+}

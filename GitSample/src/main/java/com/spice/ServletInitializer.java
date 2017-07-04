b47faf2a505bbf78636965b58ef607489d5c09c9
monika <monush2005@gmail.com>
2017-07-04 11:46:54 +0530

    added my github name

--- GitSample/src/main/java/com/spice/ServletInitializer.java
+++ GitSample/src/main/java/com/spice/ServletInitializer.java
@@ -2,7 +2,7 @@ package com.spice;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
-
+/*****/
 public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
